dd82438362e1599082beadee5edf9378007bf3f4
Kim, Allen <allen.kim@ontariohealth.ca>
2023-10-19 09:11:30 -0400

    updated

--- index.js
+++ index.js
@@ -1,31 +1,11 @@
 const express = require('express');
 const swaggerUI = require('swagger-ui-express');
-const swaggerJsDoc = require('swagger-jsdoc');
 
 const usersRouter = require('./users');
-
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Library API",
-      version: "1.0.0",
-      description: "A simple Express Library API"
-    },
-
-    servers: [
-      {
-        url: "http://localhost:3000",
-        description: "My API Documentation",
-      },
-    ],
-  },
-  apis: ["./*.js"] // this will process @swagger comments./swagger-components.js and ./users.js
-};
-const specs = swaggerJsDoc(options);
+const swaggerSpec = require('./swagger');
 
 const app = express();
 app.use("/users", usersRouter);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerSpec));
 
 app.listen(3000, () => console.log(`Server runs on port 3000, http://localhost:3000/users`));
--- swagger.js
+++ swagger.js
@@ -30,9 +30,10 @@
 
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerSpecs = swaggerJsDoc({
+  definition: {
     openapi: "3.0.0",
     info: {
-      title: "Library API",
+      title: "Users API",
       version: "1.0.0",
       description: "A simple Express Library API"
     },
